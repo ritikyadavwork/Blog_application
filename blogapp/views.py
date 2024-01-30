ecd678911d57f04ba1bb7204d68005059cdc1ae7
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-01-30 17:10:21 +0530

    write view for save the comment in database and display comment

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from .models import Post
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Post, Category, Comment
 from django.core.paginator import Paginator
 from django.db.models import Q
 
@@ -19,16 +19,22 @@ def postBlog_view(request):
         title = request.POST.get('title')
         blog = request.POST.get('blog')
         photos = request.FILES.get('images')
-        instance = Post.objects.create(title=title, blog=blog, photos=photos)
+        category_id = request.POST.get('category')
+        category = Category.objects.get(id=category_id)
+        instance = Post.objects.create(title=title, blog=blog, photos=photos, category=category)
         instance.save()
         return redirect('/blog-list')
-    return render(request, 'postblog.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'postblog.html', context)
 
 
 def blogList_view(request):
     search_post = request.GET.get('search')
     if search_post:
-        posts = Post.objects.filter(title__icontains=search_post).order_by('created')
+        posts = Post.objects.filter(Q(title__icontains=search_post) | Q(category__name__icontains=search_post))
     else:
         posts = Post.objects.all().order_by('created')
     paginator = Paginator(posts, 3)
@@ -41,8 +47,23 @@ def blogList_view(request):
 
 
 def blogDetail_view(request, post_id):
-    data = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        author = request.POST.get('author')
+        comment_text = request.POST.get('msg')
+        comment = Comment.objects.create(
+            Author=author,
+            comment=comment_text,
+            post=post,
+        )
+        comment.save()
+        return HttpResponseRedirect(request.path_info)
+
+    comments = Comment.objects.filter(post=post)
     context = {
-        'object': data
+        'object': post,
+        'comments': comments,
     }
     return render(request, 'blog_detail.html', context)
+
+
