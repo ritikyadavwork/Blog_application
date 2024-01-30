24a7f653547e85f3e1a9debdfe5d0a2790440065
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-01-30 17:08:05 +0530

    added null true and captilize category

--- blogapp/models.py
+++ blogapp/models.py
@@ -5,13 +5,14 @@ from model_utils.models import TimeStampedModel
 class Post(TimeStampedModel):
     title = models.CharField(max_length=200, null=True)
     blog = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     photos = models.ImageField(upload_to='Upload_images/')
 
     def __str__(self):
         return self.title
 
 
-class category(TimeStampedModel):
+class Category(TimeStampedModel):
     name = models.CharField(max_length=225)
 
     def __str__(self):
@@ -21,7 +22,7 @@ class category(TimeStampedModel):
 class Comment(TimeStampedModel):
     Author = models.CharField(max_length=225)
     comment = models.TextField()
-    post = models.ForeignKey("Post", on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE,  null=True)
 
     def __str__(self):
         return self.Author
