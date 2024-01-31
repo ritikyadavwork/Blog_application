97de2e73d537796eb0e74d5613200c4a39d59c32
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-01-31 18:05:27 +0530

    adde two new url update the comment

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from .views import homeView, postBlog_view, blogList_view, blogDetail_view
+from .views import homeView, postBlog_view, blogList_view, blogDetail_view, update_View, update_comment_View
 
 app_name = 'blogapp'
 
@@ -10,4 +10,7 @@ urlpatterns = [
     path('blog-view/', login_required(postBlog_view), name='blog'),
     path('blog-list/', blogList_view, name='blogDetail'),
     path('blog-deatil/<int:post_id>/', blogDetail_view, name='fullDetail'),
+    path('update/<int:comment_id>/', update_View, name='update'),
+    path('update/update-comment/<int:comment_id>/', update_comment_View, name='updateComment'),
+
 ]
