8d4943c36cf07b314eb2eb774bb0b0f1b0f7890c
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-02-01 11:56:55 +0530

    initial commit

--- blogapp/models.py
+++ blogapp/models.py
@@ -22,7 +22,7 @@ class Category(TimeStampedModel):
 class Comment(TimeStampedModel):
     Author = models.CharField(max_length=225)
     comment = models.TextField()
-    post = models.ForeignKey('Post', on_delete=models.CASCADE,  null=True)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.Author
