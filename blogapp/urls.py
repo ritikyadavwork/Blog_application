0d524ea3a9bf1bb4324ee51905341db5504097be
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-02-05 12:02:00 +0530

    set the url for delete the post using delete_id

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
-from .views import homeView, postBlog_view, blogList_view, blogDetail_view, update_View, update_comment_View
+from .views import homeView, postBlog_view, blogList_view, blogDetail_view, update_View, update_comment_View, \
+    delete_post_view
 
 app_name = 'blogapp'
 
@@ -12,5 +13,6 @@ urlpatterns = [
     path('blog-deatil/<int:post_id>/', blogDetail_view, name='fullDetail'),
     path('update/<int:comment_id>/', update_View, name='update'),
     path('update/update-comment/<int:comment_id>/', update_comment_View, name='updateComment'),
+    path('delete/<int:delete_id>/', delete_post_view, name='delete')
 
 ]
