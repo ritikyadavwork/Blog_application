476ae1499e7ae55e91f2beac181944eecd801334
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-02-06 23:50:04 +0530

    set the url to show the user profile and upload profile data

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from .views import homeView, postBlog_view, blogList_view, blogDetail_view, update_View, update_comment_View, \
-    delete_post_view
+    delete_post_view, uploadProfile_view, profile_view
 
 app_name = 'blogapp'
 
@@ -13,6 +13,7 @@ urlpatterns = [
     path('blog-deatil/<int:post_id>/', blogDetail_view, name='fullDetail'),
     path('update/<int:comment_id>/', update_View, name='update'),
     path('update/update-comment/<int:comment_id>/', update_comment_View, name='updateComment'),
-    path('delete/<int:delete_id>/', delete_post_view, name='delete')
-
+    path('delete/<int:delete_id>/', delete_post_view, name='delete'),
+    path('profile/', uploadProfile_view, name='profile'),
+    path('profile-view/', profile_view, name='profile_view'),
 ]
