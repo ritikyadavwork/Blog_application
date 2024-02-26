05321515d2ce815c6b1b20f2c78ae957c142dc10
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-02-26 17:56:55 +0530

    added the context processor to show th profile image in all templates

--- blogapp/context_processors.py
+++ blogapp/context_processors.py
@@ -2,7 +2,7 @@ from .models import Post, Category, Comment, UserProfile
 
 
 def custom_context(request):
-    images = UserProfile.objects.first()
+    images = UserProfile.objects.get()
     # images = UserProfile.objects.first()
     return {'image': images}
 
