9a3feed7332937bba3ffd5e12ff31e42f34007fe
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-02-26 17:57:52 +0530

    create the url for the show the blog on click of category

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -10,11 +10,13 @@ urlpatterns = [
     path('', homeView, name='home'),
     path('blog-view/', login_required(postBlog_view), name='blog'),
     path('blog-list/', blogList_view, name='blogDetail'),
+    path('blogs/category/<str:category>/', blogList_view, name='blog_list_category'),
     path('blog-deatil/<int:post_id>/', blogDetail_view, name='fullDetail'),
     path('update/<int:comment_id>/', update_View, name='update'),
     path('update/update-comment/<int:comment_id>/', update_comment_View, name='updateComment'),
     path('delete/<int:delete_id>/', delete_post_view, name='delete'),
     path('profile/', uploadProfile_view, name='profile'),
     path('profile-view/', profile_view, name='profile_view'),
-    path('about-us/', about_view, name='about_us')
+    path('about-us/', about_view, name='about_us'),
+
 ]
