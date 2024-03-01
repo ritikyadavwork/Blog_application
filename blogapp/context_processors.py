eb281f6e9d3a64e7eb194d1884eebe869cd0344b
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-03-01 17:49:02 +0530

    create a context and check the user is login then get the user name and show the profile image in template

--- blogapp/context_processors.py
+++ blogapp/context_processors.py
@@ -2,8 +2,17 @@ from .models import Post, Category, Comment, UserProfile
 
 
 def custom_context(request):
-    images = UserProfile.objects.get()
-    # images = UserProfile.objects.first()
-    return {'image': images}
+    context={}
+    if request.user.is_authenticated:
+        try:
+            user_profile = UserProfile.objects.get(user_profile=request.user)
+            context['image'] = user_profile
+        except:
+            pass
+    # print("user profile", user_profile)
+
+    # # images = UserProfile.objects.first()
+    return context
+
 
 # get(user=request.user)
