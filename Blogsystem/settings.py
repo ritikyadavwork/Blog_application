784051942e78d3ff579b63a7c1e7878dbb455edb
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-03-19 15:56:10 +0530

    added the whitenoise in setting file

--- Blogsystem/settings.py
+++ Blogsystem/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "allauth.account.middleware.AccountMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
 ]
 
