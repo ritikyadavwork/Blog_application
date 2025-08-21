60b6b7e4e36a2275038393c2aa418e570b9ae00e
Ritik Yadav <ritikyadavwork23@gmail.com>
2025-08-21 15:00:44 +0530

    correction in url

--- Blogsystem/urls.py
+++ Blogsystem/urls.py
@@ -19,11 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('blogapp.urls')),
+    path('inbox/notifications/', include("notifications.urls", namespace='notifications')),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
