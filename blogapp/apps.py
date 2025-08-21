13d089c1b696462ca9a05372f76b28255d86198b
Ritik Yadav <ritikyadavwork23@gmail.com>
2025-08-21 14:56:11 +0530

    added the signal in app

--- blogapp/apps.py
+++ blogapp/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BlogappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blogapp'
+
+    def ready(self):
+        from blogapp import signals
+
